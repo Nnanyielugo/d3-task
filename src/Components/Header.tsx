8cb616006b587d970cea99d96a51e6a2357cfe3a
Nnanyielugo <kz.onyegbule@gmail.com>
2021-08-05 18:12:13 +0100

    make line chart downloadable as pdf

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
+import savePdf from 'd3-save-pdf';
+import * as d3 from 'd3';
 import Box from '@material-ui/core/Box';
 import AppBar from '@material-ui/core/Toolbar';
+import Toolbar from '@material-ui/core/Toolbar';
+
 import { styled } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
 
 const CustomAppBar = styled(AppBar)({
   flexGrow: 1,
@@ -13,14 +19,34 @@ const CustomAppBar = styled(AppBar)({
 });
 
 const AppBarText = styled('h3')({
-  marginLeft: 80,
+  marginLeft: '10vw',
+});
+
+const DownloadContainer = styled('div')({
+  alignSelf: 'center',
+  marginLeft: '60vw',
 });
 
 export default function Header() {
+  const handleSavePdf = () => {
+    const config = {
+      filename: 'MySVGFile',
+    };
+    savePdf.save(d3.select('svg').node(), config);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <CustomAppBar>
-        <AppBarText>Syrup Tech Screener</AppBarText>
+        <Toolbar>
+          <AppBarText>Syrup Tech Screener</AppBarText>
+
+          <DownloadContainer>
+            <Button onClick={handleSavePdf} color="inherit" size="small">
+              <Icon>file_download</Icon>
+            </Button>
+          </DownloadContainer>
+        </Toolbar>
       </CustomAppBar>
     </Box>
   );
